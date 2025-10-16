a892b058d2ec65d817159661644c6bef8e48dbfd
Priyansh Kayastha <priyanshkayastha0803@gmail.com>
2025-10-16 17:02:16 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+
+  return (
+   <h1 className='bg-red-300 text-red-500'>Hello</h1>
+  )
+}
+
+export default App
